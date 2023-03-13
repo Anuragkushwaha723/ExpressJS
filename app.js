11bdf459b5738b227d23571ceb4a34fb6d15b8c0
Anurag Kushwaha <anuragkushwaha723@gmail.com>
2023-03-13 13:29:43 +0530

    Parsing the data by body-parser

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express=require('express');
+const bodyParser=require('body-parser');
 const app=express();
-app.use((req,res,next)=>{
-    console.log('I am in the middleware!');
-    next();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Save the Product</button></form>')
 });
-app.use((req,res,next)=>{
-    console.log('I am again in middleware!');
+
+
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req,res,next)=>{
     res.send('<h1>Welcome to my express application');
 });
-app.listen(3000);
+
+app.listen(4000);
