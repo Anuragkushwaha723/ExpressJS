803f8f458e4f142d31a203d5f41ccd647f45a84f
Anurag Kushwaha <anuragkushwaha723@gmail.com>
2023-03-15 14:29:34 +0530

    adding the routes

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 const express=require('express');
+const adminRouter=require('./routes/admin');
+const shopRouter=require('./routes/shop');
 const bodyParser=require('body-parser');
+
 const app=express();
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Save the Product</button></form>')
-});
+app.use('/admin',adminRouter);
+
+app.use('/shop',shopRouter);
 
 
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Welcome to my express application');
-});
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page Not Found </h1>');
+})
 
 app.listen(4000);
