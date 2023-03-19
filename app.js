1e08e3ddacfd7d9ed815724ca35320bdc1919d66
AnuragKushwaha <anuragkushwaha723@gmail.com>
2023-03-19 21:37:33 +0530

    Serving static file

--- app.js
+++ app.js
@@ -2,18 +2,20 @@ const express=require('express');
 const adminRouter=require('./routes/admin');
 const shopRouter=require('./routes/shop');
 const bodyParser=require('body-parser');
-
+const path=require('path');
+const rootDir=require('./util/path');
 const app=express();
+
 
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/admin',adminRouter);
 
 app.use('/shop',shopRouter);
-
+app.use(express.static(path.join(__dirname,'public')));
 
 app.use((req,res,next)=>{
-    res.status(404).send('<h1>Page Not Found </h1>');
+    res.status(404).sendFile(path.join(rootDir,'views','404.html'));
 })
 
 app.listen(4000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,10 @@
 const express=require('express');
-
+const path=require('path');
+const rootDir=require('../util/path');
 const router=express.Router();
 
 router.get('/add-product',(req,res,next)=>{
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Save the Product</button></form>')
+    res.sendFile(path.join(rootDir,'views','add-product.html'));
 });
 
 
